f860c769ff9933bc74a37cbf1f0530a7a93cd4d4
samlan24 <muchemionline@gmail.com>
2024-08-05 15:19:51 +0300

    updated files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""
+this is the main app
+"""
+
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -6,10 +13,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """Closes the current session"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+
+"""
+index module
+"""
+
 from api.v1.views import app_views
+
 
 @app_views.route('/status')
 def status():
