aa3fbb73d7a630adde56fd688d78c6e2e0b86f35
samlan24 <muchemionline@gmail.com>
2024-08-05 15:44:29 +0300

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ this is the main app
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,6 +21,6 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,12 @@
 """
 index module
 """
-
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
     """Returns status"""
-    return {"status": "OK"}
+    response = {"status": "OK"}
+    return jsonify(response)
